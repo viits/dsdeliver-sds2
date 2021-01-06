526f811bc2f4a300c26de615b2dc864cff429f3e
Esdras Victor <toyobr@hotmail.com>
2021-01-05 23:35:00 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -45,5 +45,14 @@ public class OrderService {
 		return new OrderDTO(order); 
 
 	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+	
 
 }
