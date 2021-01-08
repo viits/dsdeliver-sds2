4e6d3594dd32de2828004d3cc21d5b54c9417445
Esdras Victor <toyobr@hotmail.com>
2021-01-08 09:52:22 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home/Index';
 import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
